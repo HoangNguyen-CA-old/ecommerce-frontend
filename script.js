1b4439a40579bb93696f7dbba875b27c60fe7e54
Hoang Nguyen <hoang.viet.nguyen@outlook.com>
2020-05-15 17:52:27 -0400

    finished navbar for frontend

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let hamburger = document.querySelector('#hamburger');
 let navLinks = document.querySelector('#navLinks');
+let navbarClose = document.querySelector('#navbarClose');
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.add('navbar__links--active');
+});
+
+navbarClose.addEventListener('click', () => {
+  console.log('wow');
+  navLinks.classList.remove('navbar__links--active');
 });
