6e09e6a4d50167e4417fe66120efaa73d03d3151
HoangNguyen-CA <hoang.viet.nguyen@outlook.com>
2020-05-20 23:06:20 -0400

    finished functionality of shopping-cart, need to refactor and polish code

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ let modal = document.querySelector('#modal');
 let modalBackdrop = document.querySelector('#modalBackdrop');
 let modalClose = document.querySelector('#modalClose');
 
+let shoppingContainer = document.querySelector('#shoppingContainer');
+
 let cart = new Map(); //id => amount
+let taxRatio = 0.1;
 
 //products supposed to be fetched from database
 let products = [
@@ -38,8 +41,114 @@ let products = [
   },
 ];
 
+function addToCart(id) {
+  let item = cart.get(id);
+  let amount;
+  if (item != null) {
+    amount = item.amount + 1;
+  } else {
+    amount = 1;
+  }
+
+  let display = document.querySelector(`#display-${id}`);
+  display.innerHTML = `${amount} item(s) in cart`;
+  cart.set(id, { amount, total: 0 });
+}
+
+function updateCartTotal() {
+  for (let [key, val] of cart) {
+    let product = getProduct(key);
+
+    let total = (val.amount * product.price).toFixed(2);
+    cart.set(key, { ...val, total });
+  }
+}
+
+function updateTax() {
+  let taxPrice = document.querySelector('#taxPrice');
+  let totalPrice = document.querySelector('#totalPrice');
+
+  let total = 0;
+  for (let [key, val] of cart) {
+    total += +val.total;
+  }
+
+  let taxTotal = total * taxRatio;
+  taxPrice.innerHTML = taxTotal.toFixed(2);
+  totalPrice.innerHTML = (total + taxTotal).toFixed(2);
+}
+
+function initCart() {
+  updateCartTotal();
+
+  let content = `
+  <tr>
+    <th>Name</th>
+    <th>Price</th>
+    <th>#</th>
+    <th>Total</th>
+    <th>Remove</th>
+  </tr>
+  `;
+
+  for (let [key, val] of cart) {
+    let id = +key;
+    let product = getProduct(id);
+    if (val.amount > 0) {
+      content += `
+          <tr class="table__item">
+            <td>${product.name}</td>
+            <td>$${product.price}</td>
+            <td><input type="number" class="modal__item-amount" onchange="updateCartValue(${id}, this.value)" value="${val.amount}"/></td>
+            <td>$${val.total}</td>
+            <td><button onclick="deleteCartItem(${id})" class="modal__item-remove">remove</button></td>
+          </tr>
+      `;
+    }
+  }
+  content += `
+  <tr>
+    <td>Tax (10%)</td>
+    <td></td>
+    <td></td>
+    <td id="taxPrice"></td>
+    <td></td>
+  </tr>
+  <tr>
+    <th>Total Cost</th>
+    <th></th>
+    <th></th>
+    <th id="totalPrice"></th>
+    <th></th>
+  </tr>`;
+
+  shoppingContainer.innerHTML = content;
+  updateTax();
+}
+
+function updateCartValue(id, value) {
+  let item = cart.get(id);
+
+  let display = document.querySelector(`#display-${id}`);
+  display.innerHTML = `${value} item(s) in cart`;
+  cart.set(id, { ...item, amount: value });
+
+  initCart();
+}
+
+function deleteCartItem(id) {
+  cart.set(id, { amount: 0, total: 0 });
+  initCart();
+}
+
+function getProduct(id) {
+  for (let product of products) {
+    if (product.id === id) return product;
+  }
+  throw new Error(`Product with an id of ${id} not found`);
+}
+
 function addProductToDOM(product) {
-  console.log('RAN');
   let el = document.createElement('div');
   el.classList.add('product__item');
   let image = document.createElement('img');
@@ -55,7 +164,8 @@ function addProductToDOM(product) {
   </h6>
   <p class="product__desc">${product.desc}</p>
   <p class="product__price">$${product.price}</p>
-  <button class="button button--buy">ADD TO CART</button>
+  <button class="button button--buy" onclick="addToCart(${product.id})">ADD TO CART</button>
+  <p id="display-${product.id}"></p>
   `;
   el.appendChild(main);
   productsContainer.appendChild(el);
@@ -72,11 +182,11 @@ hamburger.addEventListener('click', () => {
 });
 
 navbarClose.addEventListener('click', () => {
-  console.log('wow');
   navLinks.classList.remove('navbar__links--active');
 });
 
 shoppingCart.addEventListener('click', () => {
+  initCart();
   modal.classList.add('modal--active');
   modalBackdrop.classList.add('modal__backdrop--active');
 });
